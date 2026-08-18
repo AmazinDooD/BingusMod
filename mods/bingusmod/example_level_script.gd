a09a7edc17cf3499d8ce2fab68049db6f8e53b9f
AmazinDooD <153538539+AmazinDooD@users.noreply.github.com>
2026-08-18 20:31:55 +0100

    OOPS thats out of date

--- mods/bingusmod/example_level_script.gd
+++ mods/bingusmod/example_level_script.gd
@@ -1,17 +1,19 @@
 extends LevelScript
 # ↑ Don't remove this line. ↑
 
-# NOTE: Level scripts are re-run when the level is restarted.
+# Note: Level scripts are re-run when the level is restarted.
+# Note 2: There's a bunch of cool functions in level_script.gd.
 
 # Called when the script is loaded, like any other node :P
 func _ready() -> void:
 	print("example level script: loaded")
 	
-	# Make anything with a pure red modulate kill you when collecting it
-	var evilStuff = get_things(FILTER.by_modulate, Color(1.0, 0.0, 0.0))
-	for thing: Area2D in evilStuff:
-		var obj = thing as Collectible
-		if not obj: continue # Object isn't a collectible
-		
-		# Override old collect function.
-		obj.collect = func(body): death()
+	# Make all collectibles with a pure red modulate kill you when collecting it
+	var evilStuff = get_things(FILTER.combine.call([
+		FILTER.collectibles_only,
+		FILTER.by_modulate,
+	]), Color(1.0, 0.0, 0.0))
+
+	for obj: Collectible in evilStuff:
+		# Override old collect function. 
+		patch_connection(obj, &"body_entered", func(_body): death(), false, [obj.collect])
